8741d3b9ef5984aa5b5056c0191fd6a5c1cbd649
IlyaBalashov <balashovia@bk.ru>
2020-05-14 09:17:05 +0300

    6 файлов удалено

--- task1.2.py
+++
@@ -1,5 +0,0 @@
-user_seconds = int(input('Введите количество секунд '))
-hours = user_seconds // 3600
-minutes = (user_seconds % 3600) // 60
-seconds = user_seconds % 60
-print(f'Введенное время: {hours:02d}:{minutes:02d}:{seconds:02d}')
--- task1.3.py
+++
@@ -1,3 +0,0 @@
-n = input('Введите число n ')
-number = (int(n) + int(n + n) + int(n + n + n))
-print(f'Сумма выражения "n + nn + nnn" равна = {number}')
