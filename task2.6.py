1f2b61e4249d84db7711920fe62a5f097d6ea175
IlyaBalashov <balashovia@bk.ru>
2020-05-19 23:56:22 +0300

    Алгоритм доработан. Аналитика работает

--- task2.6.py
+++ task2.6.py
@@ -5,15 +5,30 @@ price = input('Введите цену продукта ')
 quantity = input('Введите количество ')
 unit = input('Введите единицы измерения ')
 
-product.append((i, {'название': title, "цена": price, 'количество': quantity, 'ед.измерения': unit}))
-
-print(product)
+product.append((i, {'название': title, 'цена': price, 'количество': quantity, 'ед.измерения': unit}))
 
 while True:
-    user_ask = input('Продолжить ввод? ')
+    user_ask = input('Продолжить ввод? Для выхода - ответьте "нет"')
     if user_ask.lower() == 'нет':
-        print(f'Название - {(product[i - 1][1]).get(title)}')
-        print(f'Цена - {(product[i - 1][1]).get(price)}')
-        print(f'Количество - {(product[i - 1][1]).get(quantity)}')
-        print(f'Ед.измерения - {(product[i - 1][1]).get(unit)}')
         break
+    i += 1
+    title = input('Введите название продукта ')
+    price = input('Введите цену продукта ')
+    quantity = input('Введите количество ')
+    unit = input('Введите единицы измерения ')
+    product.append((i, {'название': title, 'цена': price, 'количество': quantity, 'ед.измерения': unit}))
+
+
+def print_result(value):
+    result = []
+    i = 0
+    while i < len(product):
+        result.append(product[i][1].get(value))
+        i += 1
+    print(f"{value} - {result}")
+
+
+print_result('название')
+print_result('цена')
+print_result('количество')
+print_result('ед.измерения')
