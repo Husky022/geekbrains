ca3ff32633eaa2c36d73d0e269abb02f4e44d617
Ilya Balashov <balashovia@bk.ru>
2020-05-20 20:11:40 +0300

    Откорректированы задачи 2.5 и 2.6
    Задача 2.5 выполнена подобно тому, что разбирали на уроке.
    Задача 2.6 доработана, аналитика выполняется

--- task2.5.py
+++ task2.5.py
@@ -1,18 +1,13 @@
-my_range = [7, 5, 3, 3, 2]
-
+my_range = [7, 7, 5, 5, 3, 3, 2, 2]
+print(my_range)
 
 while True:
     el = int(input('Введите новый элемент рейтинга '))
-
     i = 1
     while i <= len(my_range):
-        if my_range[0] < el:
-            my_range.insert(0, el)
-        elif my_range[i - 1] >= el > my_range[i]:
-            my_range.insert(i - 1, el)
-            break
-        elif my_range[len(my_range) - 1] >= el:
-            my_range.append(el)
+        if el > my_range[i-1]:
             break
         i += 1
+    my_range.insert(i - 1, float(el))
+
     print(my_range)
